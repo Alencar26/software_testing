5becfe3f0deed1d3db539966754ef330cb1fef83
Andre Alencar <aaugusto310@gmail.com>
2022-03-29 02:44:40 -0300

    apenas criando instância do navegador e abrindo um página web.

--- Selenium/src/main/java/TesteGoogle.java
+++ Selenium/src/main/java/TesteGoogle.java
@@ -1,12 +1,19 @@
+import org.junit.Assert;
+import org.junit.Test;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
 
 public class TesteGoogle {
 
-    public static void main(String[] args) {
+    @Test
+    public void teste() {
 
-        System.setProperties("webdriver.gecko.driver", "~/www/Selenium/geckodriver/geckodriver");
+        System.setProperty("webdriver.gecko.driver", "/home/alencar/www/Selenium/geckodriver/geckodriver");
         WebDriver driver = new FirefoxDriver();
+
         driver.get("https://www.google.com.br");
+        Assert.assertEquals("Google", driver.getTitle());
+
+        driver.close();
     }
 }
